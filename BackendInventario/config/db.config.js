b2db909afd0b28c5de17b06cb336528b2372450f
jhonatancancarip <jhonatancancarip@gmail.com>
2025-10-02 09:36:33 -0400

    config.actualisado

--- BackendInventario/config/db.config.js
+++ BackendInventario/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    BD: "inventario",
+    HOST: "b7elpkydmy78h2ar0wkd-mysql.services.clever-cloud.com",
+    USER: "ubpeh39iwyjyf894",
+    PASSWORD: "GW39jBQzs6uLfQy3isMp",
+    BD: "b7elpkydmy78h2ar0wkd",
     DIALECT: "mysql",
     Port: 3306,
 }  
